1c14c45f9ad123bbdbf0d0101b407b1202a6ea5a
Lucas Vascovici <lucas.vascovici@epita.fr>
2019-04-12 19:01:41 +0200

    read json

--- build.sbt
+++ build.sbt
@@ -5,3 +5,4 @@ version := "1.0"
 scalaVersion := "2.12.8"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5"
+libraryDependencies += "com.typesafe.play"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
+import play.api.libs.json.Json
+import scala.io.Source
+
 object Main {
 	def main(args: Array[String]): Unit = {
-		println("Hello NY")
+
+		val json = Json.parse(Source.fromFile("test.json").getLines().mkString)
+		val lat = (json \ "latitude").get
+		println(lat)
 	}
 }
