2b3f84f106c2452fcaac87cad02dbefc5e5de142
Lucas Vascovici <lucas.vascovici@epita.fr>
2019-05-10 14:52:11 +0200

    lecture des dossiers

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,26 @@
 import scala.io.Source
 import scalaj.http._
+import java.io.File
 
 object Main {
 
+	def getListOfFiles(dir: String):List[File] = {
+		val d = new File(dir)
+		if (d.exists && d.isDirectory) {
+			d.listFiles.filter(_.isFile).toList
+		} else {
+			List[File]()
+		}
+	}
+
 	def sendJsonString(s: String): Any = {
-			val req = Http("http://localhost:9000/saveDrone").postData(s).header("Content-type", "application/json").asString.code
+		val req = Http("http://localhost:9000/saveDrone").postData(s).header("Content-type", "application/json").asString.code
 	}
 
 	def main(args: Array[String]): Unit = {
-		val lines = Source.fromFile("jsons/test.txt").getLines()
-		lines.map(sendJsonString).toList
+		val paths = args.flatMap(getListOfFiles)
+		val files = paths.flatMap(Source.fromFile(_).getLines())
+		files.map(sendJsonString).toList
+
 	}
 }
